06f1f1c708df9b89bc02600e942f7710c9078395
carlos <huaixian.huang@gmail.com>
2018-05-10 15:24:51 +0800

    Feat: work now

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # org-agenda-carlos
 carlos customize org agendas for GTD
+
+## key binding
+
+(global-set-key (kbd "<f5>") 'carlos/org-agenda-show)
+
+## org file lists
+
+(setq carlos/org-agenda-file-list '("path/of/orgfile"))
+
+## customize org agenda command
+
+append the customize commands to origin org agendas command, need to set in emacs init stage
+
+(setq org-agenda-custom-commands (append org-agenda-custom-commands org-agenda-custom-commands))
+
+
+## quick capture note
+
+```
+(setq org-capture-templates
+      '(
+        ("ll" "Leju 日常" entry (file+datetree "path/orgfile/store/note/also/in/carlos/org-genda-file-list")
+         "* TODO [#A] [Leju 日常] %?\n:PROPERTIES:\n:CREATED:  %U\n:END:\n")
+        ))      
+```
--- org_agenda_carlos.el
+++ org_agenda_carlos.el
@@ -1,14 +1,14 @@
 ;;; -*- lexical-binding: t -*-
-(setq org-agenda-custom-commands
+(setq org-carlos-agenda-custom-commands
       '(
-        ("carlos" "carlos work panel"
+        ("carlos/org-agenda" "carlos work panel"
          (
           (tags "+UNHOLD+TODO=\"WORKING\"|-HOLD+LejuWork+TODO=\"WORKING\"|+Work+TODO=\"IN-PROGRESS\"|-HOLD+LejuWork+TODO=\"TARGET\""
                 (
                  (org-agenda-overriding-header "❖----------------LONG-TREM & Working----------------------❖")
                  (org-agenda-prefix-format "%l%t")
                  (org-agenda-sorting-strategy '(category-keep))
-                 (org-agenda-files carlos/leju-org-agenda-file-list)
+                 (org-agenda-files carlos/org-agenda-file-list)
                  )
                 )
           (agenda "schedule"
@@ -18,18 +18,41 @@
                    (org-agenda-span 1)
                    (org-agenda-start-day "+0d")
                    (org-agenda--show-holidays-birthdays t)
-                   (org-agenda-files carlos/leju-org-agenda-file-list)
+                   (org-agenda-files carlos/org-agenda-file-list)
                    ))
           (tags "-HOLD+LejuWork+TODO=\"TODO\"|-HOLD+LejuWork+TODO=\"WORKING\""
                 (
                  (org-agenda-overriding-header "❖----------------TODO lists----------------------❖")
                  (org-agenda-cmp-user-defined 'org-sort-agenda-items-sort-created)
                  (org-agenda-sorting-strategy '(user-defined-up))
-                 (org-agenda-files carlos/leju-org-agenda-file-list)
+                 (org-agenda-files carlos/org-agenda-file-list)
                  (org-agenda-skip-function 'carlos/org-agenda-filter-schedule-todo)
-                 (org-agenda-before-sorting-filter-function 'carlos/org-agenda-before-sorting-filter-function)
-                 )
-                )
-          ))
-        ))
+                 (org-agenda-before-sorting-filter-function 'carlos/org-agenda-before-sorting-filter-function)))))))
+
+(defun org-sort-agenda-items-sort-created (a b)
+  (let (
+        (a-pos (get-text-property 0 'org-marker a))
+        (b-pos (get-text-property 0 'org-marker b))
+        )
+    (let ((a-priority (* (- 255 (string-to-char (org-entry-get a-pos "PRIORITY"))) 100000))
+          (b-priority (* (- 255 (string-to-char (org-entry-get b-pos "PRIORITY"))) 100000)))
+      (let ((a-time (+ a-priority (time-to-number-of-days (carlos/org-agenda-parsetime (or (org-entry-get a-pos "CREATED") "[1970-01-02 Sun 00:01]")))))
+            (b-time (+ b-priority (time-to-number-of-days (carlos/org-agenda-parsetime (or (org-entry-get b-pos "CREATED") "[1970-01-02 Sun 00:01]"))))))
+        (if (time-less-p b-time a-time)
+            (progn
+              -1
+              ) nil)))))
+
+(defun carlos/org-agenda-show (&optional arg)
+  (interactive )
+  (progn
+    (setq frame-title-format "DashBoard")
+    (org-agenda arg "carlos/org-agenda")
+    (when (< 1 (length (window-list)))
+      (delete-other-windows))))
+
+(defun carlos/org-agenda-parsetime (timestr)
+  (let ((time1 (parse-time-string (or  timestr (format-time-string "%Y-%m-%d")))))
+    (encode-time (or (nth 0 time1) 0) (or (nth 1 time1) 0) (or (nth 2 time1) 0) (nth 3 time1) (nth 4 time1) (nth 5 time1))))
+
 (provide 'org_agenda_carlos)
